f1da638709f5ad5f3e47e6dcff64b8a3c510d9ca
Morteza Karimi <mortezak1373@gmail.com>
2024-03-31 13:29:16 +0330

    chore: add shield.io /endpoint response format

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 // Tiny node server to make Render deploy status available
 
 // TODO: Should we only call dotenv in DEV mode?
-require('dotenv').config();
+require('dotenv')
+  .config();
 const express = require('express');
 const compression = require('compression');
 const api = require('api');
+const { titleCase } = require('tiny-case');
 const debug = require('debug')('main');
 
 const app = express();
 app.use(compression());
 
-const renderApi = api('@render-api/v1.0#1ifry11lo91zxhg');
+const renderApi = api('@render-api/v1.0#34i64rhilu8ilhkj');
 renderApi.auth(process.env.API_KEY);
 
+// Display strings
+const unknown = 'unknown';
+const inProgress = 'in progress';
+const success = 'success';
+const failed = 'failed';
 const status = (deployStatus) => {
-  // Display strings
-  const unknown = 'unknown';
-  const inProgress = 'in progress';
-  const success = 'success';
-  const failed = 'failed';
-
   // If we are given a key other than a string, we don't know what's going on
   if (typeof deployStatus !== 'string') {
     return unknown;
@@ -43,6 +44,15 @@ const status = (deployStatus) => {
   return statusMap[deployStatus] ?? unknown;
 };
 
+const color = (statusDisplay) => {
+  const statusColorMap = {
+    [inProgress]: 'important',
+    [success]: 'success',
+    [failed]: 'critical',
+  };
+  return statusColorMap[statusDisplay] ?? 'inactive';
+};
+
 app.get('/:id?', (req, res) => {
   const serviceId = req.params.id;
 
@@ -55,7 +65,10 @@ app.get('/:id?', (req, res) => {
   }
   debug(`Getting deploys for Render service: ${serviceId}`);
 
-  renderApi.getDeploys({ limit: '1', serviceId })
+  renderApi.getDeploys({
+    limit: '1',
+    serviceId,
+  })
     .then(({ data }) => {
       res.json({
         status: status(data[0]?.deploy?.status),
@@ -65,6 +78,51 @@ app.get('/:id?', (req, res) => {
       res.json({
         status: status(null), // Unknown
       });
+      debug(err);
+    });
+});
+app.get('/endpoint/:id?', (req, res) => {
+  const serviceId = req.params.id;
+
+  const responseObject = {
+    schemaVersion: 1,
+    label: 'Render Unknown Service',
+    message: status(null),
+    color: color(null),
+    namedLogo: 'render',
+    isError: true,
+  };
+  if (!serviceId) {
+    res.json(responseObject);
+    debug('No service ID in url');
+    return;
+  }
+  renderApi.getService({ serviceId })
+    .then(({ data }) => {
+      const serviceName = titleCase(data.name);
+      debug(`Getting deploys for Render service: ${serviceName}(${serviceId})`);
+      responseObject.label = `Render ${serviceName}`;
+      renderApi.getDeploys({
+        limit: '1',
+        serviceId,
+      })
+        .then(({ data }) => {
+          responseObject.message = status(data[0]?.deploy?.status);
+          responseObject.color = color(responseObject.message);
+          responseObject.isError = false;
+          res.json(responseObject);
+        })
+        .catch((err) => {
+          responseObject.message = status(null);
+          responseObject.color = color(null);
+          res.json(responseObject);
+          debug(err);
+        });
+    })
+    .catch((err) => {
+      responseObject.message = status(null);
+      responseObject.color = color(null);
+      res.json(responseObject);
       debug(err);
     });
 });
@@ -80,5 +138,6 @@ async function closeGracefully(signal) {
   await server.close();
   process.kill(process.pid, signal);
 }
+
 process.once('SIGINT', closeGracefully);
 process.once('SIGTERM', closeGracefully);
